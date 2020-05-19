bae8786a13bd75f1462a044a9b10127d3e04c76e
shiquan <shiquan@taharaiot.com>
2020-05-19 14:57:37 +0800

    ...

---
+++ src/main/java/shi/quan/RCPSPService.java
@@ -0,0 +1,11 @@
+package shi.quan;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.List;
+
+@ApplicationScoped
+public class RCPSPService<Task> {
+    public List<Task> ssgs() {
+        return null;
+    }
+}
