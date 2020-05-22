b9537cc6bbafdcf889e4bb4792ee2854998fd7e1
yystju <yystju@hotmail.com>
2020-05-22 14:56:42 +0800

    ...

--- src/main/antlr4/shi/quan/psplib/PSPLIB.g4
+++ src/main/antlr4/shi/quan/psplib/PSPLIB.g4
@@ -4,17 +4,36 @@ grammar PSPLIB;
 //    package shi.quan.psplib;
 //}
 
-model:	(line NEWLINE)* ;
+model :	(line NEWLINE)* ;
 
-line: comment | prop ;
+line : comment | resource | resourceParam | projectInformation | precedenceRelations | requestsDurations | resourceAvailabilities | prop | headers | data;
+
+resource : 'RESOURCES' ;
+
+resourceParam: WS '-' WS TEXT WS ':' WS TEXT;
+
+projectInformation: 'PROJECT INFORMATION:' ;
+
+precedenceRelations: 'PRECEDENCE RELATIONS:' ;
+
+requestsDurations: 'REQUESTS/DURATIONS:' ;
+
+resourceAvailabilities: 'RESOURCEAVAILABILITIES:' ;
+
+headers: HEADER_TEXT (WS TEXT)* ;
+
+data: NUMBER (WS NUMBER)* ;
 
 prop: key WS* ':' WS* value ;
 
 key:  TEXT (WS TEXT)* ;
 value: TEXT (WS TEXT)* ;
 
-TEXT : [a-zA-Z0-9]+ ;
-WS: [ \t]+ ;
+HEADER_TEXT : [a-zA-Z]+ ;
+WS     : [ \t]+ ;
+LETTER : [a-zA-Z]+ ;
+NUMBER : [0-9]+;
+TEXT   : [a-zA-Z0-9.(/)_#]+ ;
 
 comment : '*' '*'+ ;
 
--- src/test/java/shi/quan/PSPLIBParserTest.java
+++ src/test/java/shi/quan/PSPLIBParserTest.java
@@ -1,20 +1,91 @@
 package shi.quan;
 
 import io.quarkus.test.junit.QuarkusTest;
-import org.jgrapht.Graph;
-import org.jgrapht.graph.DefaultEdge;
-import org.jgrapht.graph.builder.GraphTypeBuilder;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import shi.quan.psplib.PSPLIBBaseListener;
+import shi.quan.psplib.PSPLIBLexer;
+import shi.quan.psplib.PSPLIBParser;
 
-import javax.inject.Inject;
+import java.io.IOException;
+import java.io.InputStream;
 
 @QuarkusTest
 public class PSPLIBParserTest {
     private static final Logger logger = LoggerFactory.getLogger(PSPLIBParserTest.class);
 
     @Test
-    public void test() {
+    public void test() throws IOException {
+        InputStream ins = PSPLIBParserTest.class.getResourceAsStream("/j120.sm/j1201_1.sm");
+
+        CharStream stream = CharStreams.fromStream(ins);
+
+        PSPLIBLexer lexer = new PSPLIBLexer(stream);
+
+        PSPLIBParser parser = new PSPLIBParser(new CommonTokenStream(lexer));
+
+        parser.addParseListener(new PSPLIBBaseListener() {
+            @Override
+            public void enterModel(PSPLIBParser.ModelContext ctx) {
+                logger.info("[enterModel] {}", ctx.getText());
+                super.enterModel(ctx);
+            }
+
+            @Override
+            public void enterResource(PSPLIBParser.ResourceContext ctx) {
+                logger.info("[enterResource] {}", ctx.getText());
+                super.enterResource(ctx);
+            }
+
+            @Override
+            public void enterProjectInformation(PSPLIBParser.ProjectInformationContext ctx) {
+                logger.info("[enterProjectInformation] {}", ctx.getText());
+                super.enterProjectInformation(ctx);
+            }
+
+            @Override
+            public void enterPrecedenceRelations(PSPLIBParser.PrecedenceRelationsContext ctx) {
+                logger.info("[enterPrecedenceRelations] {}", ctx.getText());
+                super.enterPrecedenceRelations(ctx);
+            }
+
+            @Override
+            public void enterRequestsDurations(PSPLIBParser.RequestsDurationsContext ctx) {
+                logger.info("[enterRequestsDurations] {}", ctx.getText());
+                super.enterRequestsDurations(ctx);
+            }
+
+            @Override
+            public void enterResourceAvailabilities(PSPLIBParser.ResourceAvailabilitiesContext ctx) {
+                logger.info("[enterResourceAvailabilities] {}", ctx.getText());
+                super.enterResourceAvailabilities(ctx);
+            }
+
+            @Override
+            public void exitHeaders(PSPLIBParser.HeadersContext ctx) {
+                logger.info("[exitHeaders] {}", ctx.getText());
+                super.enterHeaders(ctx);
+            }
+
+            @Override
+            public void exitData(PSPLIBParser.DataContext ctx) {
+                logger.info("[exitData] {}", ctx.getText());
+                super.enterData(ctx);
+            }
+
+            @Override
+            public void exitProp(PSPLIBParser.PropContext ctx) {
+                logger.info("[exitProp] {}", ctx.getText());
+                super.enterProp(ctx);
+            }
+        });
+
+        PSPLIBParser.ModelContext context = parser.model();
+
+        ins.close();
     }
 }
