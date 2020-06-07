14ea2f5955d9b2fb788781d43670a33c031e8f41
yystju <yystju@hotmail.com>
2020-06-07 20:59:07 +0800

    ...

--- src/test/java/shi/quan/PSPLIBParserTest.java
+++ src/test/java/shi/quan/PSPLIBParserTest.java
@@ -17,17 +17,26 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 @QuarkusTest
 public class PSPLIBParserTest {
     private static final Logger logger = LoggerFactory.getLogger(PSPLIBParserTest.class);
+    public static final String RESOURCE = "RESOURCE";
+    public static final String PROJECTINFORMATION = "PROJECTINFORMATION";
+    public static final String PRECEDENCERELATIONS = "PRECEDENCERELATIONS";
+    public static final String REQUESTSDURATIONS = "REQUESTSDURATIONS";
+    public static final String RESOURCEAVAILABILITIES = "RESOURCEAVAILABILITIES";
+
+    private static boolean verbose = false;
 
     @Test
     public void test() throws IOException {
         InputStream ins = PSPLIBParserTest.class.getResourceAsStream("/j120.sm/j1201_1.sm");
-//        InputStream ins = PSPLIBParserTest.class.getResourceAsStream("/example.sm");
 
         PSPData data = new PSPData();
+
+        Stack<String> stack = new Stack<>();
 
         CharStream stream = CharStreams.fromStream(ins);
 
@@ -36,50 +45,92 @@ public class PSPLIBParserTest {
         PSPLIBParser parser = new PSPLIBParser(new CommonTokenStream(lexer));
 
         parser.addParseListener(new PSPLIBBaseListener() {
-            String current = "";
             String privateKey = null;
-
 
             @Override
             public void enterResource(PSPLIBParser.ResourceContext ctx) {
-//                logger.info("[enterResource] {}", ctx.getText());
-                current = "RESOURCE";
+                if(verbose) logger.info("[enterResource] {}", ctx.getText());
+                stack.push(RESOURCE);
                 super.enterResource(ctx);
             }
 
             @Override
+            public void exitResource(PSPLIBParser.ResourceContext ctx) {
+                super.exitResource(ctx);
+                stack.pop();
+            }
+
+            @Override
             public void enterProjectInformation(PSPLIBParser.ProjectInformationContext ctx) {
-//                logger.info("[enterProjectInformation] {}", ctx.getText());
-                current = "PROJECTINFORMATION";
+                if(verbose) logger.info("[enterProjectInformation] {}", ctx.getText());
+                stack.push(PROJECTINFORMATION);
                 super.enterProjectInformation(ctx);
             }
 
             @Override
+            public void exitProjectInformation(PSPLIBParser.ProjectInformationContext ctx) {
+                super.exitProjectInformation(ctx);
+                stack.pop();
+            }
+
+            @Override
             public void enterPrecedenceRelations(PSPLIBParser.PrecedenceRelationsContext ctx) {
-                logger.info("[enterPrecedenceRelations] {}", ctx.getText());
-                current = "PRECEDENCERELATIONS";
+                if(verbose) logger.info("[enterPrecedenceRelations] {}", ctx.getText());
+                stack.push(PRECEDENCERELATIONS);
                 super.enterPrecedenceRelations(ctx);
             }
 
             @Override
+            public void exitPrecedenceRelations(PSPLIBParser.PrecedenceRelationsContext ctx) {
+                super.exitPrecedenceRelations(ctx);
+                stack.pop();
+            }
+
+            @Override
+            public void enterRequestsDurations(PSPLIBParser.RequestsDurationsContext ctx) {
+                if(verbose) logger.info("[enterRequestsDurations] {}", ctx.getText());
+                stack.push(REQUESTSDURATIONS);
+                super.enterRequestsDurations(ctx);
+            }
+
+            @Override
+            public void exitRequestsDurations(PSPLIBParser.RequestsDurationsContext ctx) {
+                super.exitRequestsDurations(ctx);
+            }
+
+            @Override
+            public void enterResourceAvailableilities(PSPLIBParser.ResourceAvailableilitiesContext ctx) {
+                if(verbose) logger.info("[enterResourceAvailableilities] {}", ctx.getText());
+                stack.push(RESOURCEAVAILABILITIES);
+                super.enterResourceAvailableilities(ctx);
+            }
+
+            @Override
+            public void exitResourceAvailableilities(PSPLIBParser.ResourceAvailableilitiesContext ctx) {
+                super.exitResourceAvailableilities(ctx);
+            }
+
+            @Override
             public void exitKey(PSPLIBParser.KeyContext ctx) {
-                logger.info("[exitKey] {}", ctx.getText());
+                if(verbose) logger.info("[exitKey] {}", ctx.getText());
                 privateKey = ctx.getText();
                 super.exitKey(ctx);
             }
 
             @Override
             public void exitValue(PSPLIBParser.ValueContext ctx) {
-//                logger.info("[exitValue] {}", ctx.getText());
+                if(verbose) logger.info("[exitValue] {}", ctx.getText());
 
                 String key = privateKey.trim();
                 String value = ctx.getText().trim();
 
-                if("RESOURCE".equals(current)) {
-                    data.getResources().put(key, value);
-                } else if("PROJECTINFORMATION".equals(current)) {
+                String current = stack.isEmpty() ? null : stack.peek();
 
-                } else if("PRECEDENCERELATIONS".equals(current)) {
+                if(RESOURCE.equals(current)) {
+                    data.getResources().put(key, value);
+                } else if(PROJECTINFORMATION.equals(current)) {
+
+                } else if(PRECEDENCERELATIONS.equals(current)) {
 
                 } else {
                     data.getProperties().put(key, value);
@@ -90,20 +141,21 @@ public class PSPLIBParserTest {
 
             @Override
             public void exitHeader(PSPLIBParser.HeaderContext ctx) {
-//                logger.info("[exitHeader] {}", ctx.getText());
+                if(verbose) logger.info("[exitHeader] {}", ctx.getText());
 
+                String current = stack.isEmpty() ? null : stack.peek();
                 String header = ctx.getText().trim();
 
-                if("RESOURCE".equals(current)) {
+                if(RESOURCE.equals(current)) {
 
-                } else if("PROJECTINFORMATION".equals(current)) {
+                } else if(PROJECTINFORMATION.equals(current)) {
                     String[] columns = parseHeader(ctx);
-                    logger.info("[PROJECTINFORMATION] columns : {}, header : {}", columns, header);
+                    if(verbose) logger.info("[PROJECTINFORMATION] columns : {}, header : {}", columns, header);
 
                     data.setProjectInformationHeader(header);
-                } else if("PRECEDENCERELATIONS".equals(current)) {
+                } else if(PRECEDENCERELATIONS.equals(current)) {
                     String[] columns = parseHeader(ctx);
-                    logger.info("[PRECEDENCERELATIONS] columns : {}, header : {}", columns, header);
+                    if(verbose) logger.info("[PRECEDENCERELATIONS] columns : {}, header : {}", columns, header);
 
                     data.setPrecedenceRelationsHeader(header);
                 } else {
@@ -115,19 +167,19 @@ public class PSPLIBParserTest {
 
             @Override
             public void exitRow(PSPLIBParser.RowContext ctx) {
-//                logger.info("[exitRow] {}", ctx.getText());
-
+                if(verbose) logger.info("[exitRow] {}", ctx.getText());
+                String current = stack.isEmpty() ? null : stack.peek();
                 String row = ctx.getText().trim();
 
                 if("RESOURCE".equals(current)) {
 
-                } else if("PROJECTINFORMATION".equals(current)) {
+                } else if(PROJECTINFORMATION.equals(current)) {
                     String[] columns = parseRow(ctx);
-                    logger.info("[PROJECTINFORMATION] columns : {}, row : {}", columns, row);
+                    if(verbose) logger.info("[PROJECTINFORMATION] columns : {}, row : {}", columns, row);
                     data.getProjectInformationRows().add(row);
-                } else if("PRECEDENCERELATIONS".equals(current)) {
+                } else if(PRECEDENCERELATIONS.equals(current)) {
                     String[] columns = parseRow(ctx);
-                    logger.info("[PRECEDENCERELATIONS] columns : {}, row : {}", columns, row);
+                    if(verbose) logger.info("[PRECEDENCERELATIONS] columns : {}, row : {}", columns, row);
                     data.getPprecedenceRelationsRows().add(row);
                 } else {
 
@@ -138,13 +190,13 @@ public class PSPLIBParserTest {
 
             @Override
             public void exitOthers(PSPLIBParser.OthersContext ctx) {
-//                logger.info("[exitOthers] {}", ctx.getText());
+                if(verbose) logger.info("[exitOthers] {}", ctx.getText());
                 super.exitOthers(ctx);
             }
 
             @Override
             public void visitErrorNode(ErrorNode node) {
-//                logger.info("[visitErrorNode] {}", node.getText());
+                if(verbose) logger.info("[visitErrorNode] {}", node.getText());
                 super.visitErrorNode(node);
             }
         });
