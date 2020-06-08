f05d249d528c434e2048c886fef5a01c4e45343f
yystju <yystju@hotmail.com>
2020-06-09 00:03:55 +0800

    ...

--- run.cmd
+++ run.cmd
@@ -1,2 +1,2 @@
-@rem java -jar .\target\quarkustest2-1.0.0-SNAPSHOT-runner.jar .\workspace\j120.sm\j1201_1.sm
-.\target\quarkustest2-1.0.0-SNAPSHOT-runner .\workspace\j120.sm\j1201_1.sm
+java -jar .\target\quarkustest2-1.0.0-SNAPSHOT-runner.jar .\workspace\j120.sm\j1201_1.sm
+@rem .\target\quarkustest2-1.0.0-SNAPSHOT-runner .\workspace\j120.sm\j1201_1.sm
