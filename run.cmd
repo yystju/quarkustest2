03d5777fca58b014f3604368053a6144407b6ad7
yystju <yystju@hotmail.com>
2020-06-09 00:11:42 +0800

    ...

--- run.cmd
+++ run.cmd
@@ -1,2 +1,2 @@
-java -jar .\target\quarkustest2-1.0.0-SNAPSHOT-runner.jar .\workspace\j120.sm\j1201_1.sm
+for %%y in (.\workspace\j120.sm\*) DO java -jar .\target\quarkustest2-1.0.0-SNAPSHOT-runner.jar %%y
 @rem .\target\quarkustest2-1.0.0-SNAPSHOT-runner .\workspace\j120.sm\j1201_1.sm
